3b9a199a95a34ea39e0e6ffe281fa0039bf74f3b
Samarth Mayya <samarthmayya@gmail.com>
2021-02-23 13:37:18 +0530

    First marketplace commit

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  def inc_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
   def index
-    @products = Product.order(:title) 
+    @products = Product.order(:title)
+    @count = inc_counter
   end
 end
